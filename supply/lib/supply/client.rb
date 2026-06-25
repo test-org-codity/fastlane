4974e83e05ec5b22e20816cdb5aed5678e33609f
Dhiren-codity <dhiren-codity@example.com>
2026-06-26 02:11:46 +0530

    in

--- supply/lib/supply/client.rb
+++ supply/lib/supply/client.rb
@@ -177,13 +177,23 @@ module Supply
       if edit_id
         UI.verbose("Getting edit with id #{edit_id} with package name #{package_name}…")
         begin
-          get_edit(package_name: package_name, edit_id: edit_id || @current_edit&.id)
+          get_edit(package_name: package_name, edit_id: edit_id)
           UI.verbose("Got edit with id #{edit_id} with package name #{package_name}.")
         rescue => e
           # Can't really rescue from a specific error here, because we are already unwrapping the Google API client error in `call_google_api`.
           # Reasons for failure could be if the edit is no long active (e.g. has been deleted, superseded or expired).
           # In that case we will create a new edit.
           UI.verbose("Failed to get edit with id #{edit_id} with package name #{package_name}. Reason: #{e.message}\nCreating a new edit…")
+          begin_edit(package_name: package_name, override_current_edit: true)
+          UI.verbose("Created a new edit with id #{@current_edit.id} with package name #{package_name}.")
+        end
+      elsif @current_edit
+        UI.verbose("No edit_id provided. Trying to reuse existing edit with id #{@current_edit.id} with package name #{package_name}…")
+        begin
+          get_edit(package_name: package_name, edit_id: @current_edit.id)
+          UI.verbose("Got existing edit with id #{@current_edit.id} with package name #{package_name}.")
+        rescue => e
+          UI.verbose("Failed to get existing edit with id #{@current_edit.id} with package name #{package_name}. Reason: #{e.message}\nCreating a new edit…")
           begin_edit(package_name: package_name, override_current_edit: true)
           UI.verbose("Created a new edit with id #{@current_edit.id} with package name #{package_name}.")
         end
